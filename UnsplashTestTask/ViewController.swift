e77dbc9550d77deda0ce196a0559be6da6ca140b
kokosDev <maks.kokos.dev@gmail.com>
2022-06-21 13:31:20 +0300

    create new project

---
+++ UnsplashTestTask/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UnsplashTestTask
+//
+//  Created by Maks Kokos on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
